cd53ace83fcff53b9637f0f63def24a6efc9d8c4
Rohith Nallandigal <rnallandigal@gmail.com>
2022-12-03 00:55:42 -0500

    [Haskell] Day 03, 2022: Cleanup
    Signed-off-by: Rohith Nallandigal <rnallandigal@gmail.com>

--- haskell/src/AOC2022/Day03.hs
+++ haskell/src/AOC2022/Day03.hs
@@ -1,7 +1,7 @@
 module AOC2022.Day03 (day03a, day03b) where
 
 import           Data.Char (isAsciiLower, ord)
-import           Data.List.Split (chunksOf, splitOn)
+import           Data.List.Split (chunksOf)
 import qualified Data.Set as S
 
 tr :: Char -> Int
@@ -12,9 +12,7 @@ soln :: [[String]] -> Int
 soln = sum . map (tr . head . S.toList . foldl1 S.intersection . map S.fromList)
 
 day03a :: String -> String
-day03a = show . soln
-    . map (\x -> let n = length x `div` 2 in [take n x, drop n x])
-    . lines
+day03a = show . soln . map (\x -> chunksOf (length x `div` 2) x) . lines
 
 day03b :: String -> String
 day03b = show . soln . chunksOf 3 . lines
