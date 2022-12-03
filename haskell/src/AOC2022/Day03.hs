f6dc357463641a36e302573d95a324648b328d14
Rohith Nallandigal <rnallandigal@gmail.com>
2022-12-03 00:41:14 -0500

    [Haskell] Day 03, 2022
    Signed-off-by: Rohith Nallandigal <rnallandigal@gmail.com>

--- haskell/src/AOC2022/Day03.hs
+++ haskell/src/AOC2022/Day03.hs
@@ -1,9 +1,20 @@
 module AOC2022.Day03 (day03a, day03b) where
 
-import Data.List.Split (splitOn)
+import           Data.Char (isAsciiLower, ord)
+import           Data.List.Split (chunksOf, splitOn)
+import qualified Data.Set as S
+
+tr :: Char -> Int
+tr c | isAsciiLower c = ord c - ord 'a' + 1
+     | otherwise      = ord c - ord 'A' + 27
+
+soln :: [[String]] -> Int
+soln = sum . map (tr . head . S.toList . foldl1 S.intersection . map S.fromList)
 
 day03a :: String -> String
-day03a = const ""
+day03a = show . soln
+    . map (\x -> let n = length x `div` 2 in [take n x, drop n x])
+    . lines
 
 day03b :: String -> String
-day03b = const ""
+day03b = show . soln . chunksOf 3 . lines
