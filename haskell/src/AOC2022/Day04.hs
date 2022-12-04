7dcfafc3068f603aa7080ff0ea4464868bcf1089
Rohith Nallandigal <rnallandigal@gmail.com>
2022-12-04 11:53:08 -0500

    [Haskell] Day 04, 2022
    Signed-off-by: Rohith Nallandigal <rnallandigal@gmail.com>

--- haskell/src/AOC2022/Day04.hs
+++ haskell/src/AOC2022/Day04.hs
@@ -2,8 +2,18 @@ module AOC2022.Day04 (day04a, day04b) where
 
 import Data.List.Split (splitOn)
 
+import Lib (between, count)
+
+input :: String -> [[[Int]]]
+input = map (map (map read . splitOn "-") . splitOn ",") . lines
+
 day04a :: String -> String
-day04a = const ""
+day04a = show . count matches . input where
+    matches [[a, b], [c, d]]
+         = (c `between` (a, b) && d `between` (a, b))
+        || (a `between` (c, d) && b `between` (c, d))
 
 day04b :: String -> String
-day04b = const ""
+day04b = show . count matches . input where
+    matches [[a, b], [c, d]]
+        = a `between` (c, d) || c `between` (a, b)
