e7c8599d3967ac8aabb48f65b3d1e15354ba0d99
Rohith Nallandigal <rnallandigal@gmail.com>
2022-12-05 17:33:21 -0500

    [C++] Day 01, 2022
    Signed-off-by: Rohith Nallandigal <rnallandigal@gmail.com>

--- c++/src/2022/01.cpp
+++ c++/src/2022/01.cpp
@@ -1,9 +1,5 @@
 #include <vector>
 #include <string>
-#include <unordered_map>
-#include <unordered_set>
-#include <utility>
-#include <tuple>
 
 #include <fmt/format.h>
 
@@ -12,11 +8,29 @@
 namespace aoc2022::day01 {
 
 std::string part1(std::string const & in) {
-	return fmt::format("{}", "");
+	int max_calories = 0;
+	for(auto elves : aoc::split(in, "\n\n")) {
+		int sum_calories = 0;
+		for(auto calories : aoc::split(elves, "\n")) {
+			sum_calories += std::stoi(calories);
+		}
+		max_calories = std::max(max_calories, sum_calories);
+	}
+	return fmt::format("{}", max_calories);
 }
 
 std::string part2(std::string const & in) {
-	return fmt::format("{}", "");
+	std::vector<int> sums;
+	for(auto elves : aoc::split(in, "\n\n")) {
+		int sum_calories = 0;
+		for(auto calories : aoc::split(elves, "\n")) {
+			sum_calories += std::stoi(calories);
+		}
+		sums.push_back(sum_calories);
+	}
+	std::sort(sums.begin(), sums.end());
+	int n = sums.size();
+	return fmt::format("{}", sums[n - 3] + sums[n - 2] + sums[n - 1]);
 }
 
 }	// namespace aoc2022::day01
