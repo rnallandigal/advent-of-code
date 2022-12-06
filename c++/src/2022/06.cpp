444aba728fed874a4aa18e34c6b07ee07d0a2ef4
Rohith Nallandigal <rnallandigal@gmail.com>
2022-12-06 00:35:54 -0500

    [C++] Day 06, 2022
    Signed-off-by: Rohith Nallandigal <rnallandigal@gmail.com>

--- c++/src/2022/06.cpp
+++ c++/src/2022/06.cpp
@@ -1,9 +1,5 @@
 #include <vector>
 #include <string>
-#include <unordered_map>
-#include <unordered_set>
-#include <utility>
-#include <tuple>
 
 #include <fmt/format.h>
 
@@ -11,12 +7,25 @@
 
 namespace aoc2022::day06 {
 
+int solve(std::string const & in, int length) {
+	std::vector<int> seen(26);
+	int l = 0, r = 0;
+	while(r < (int)in.size() && r - l < length) {
+		if(seen[in[r] - 'a']) {
+			while(in[l] != in[r]) seen[in[l++] - 'a'] = 0;
+			seen[in[l++] - 'a'] = 0;
+		}
+		seen[in[r++] - 'a'] = 1;
+	}
+	return r;
+}
+
 std::string part1(std::string const & in) {
-	return fmt::format("{}", "");
+	return fmt::format("{}", solve(in, 4));
 }
 
 std::string part2(std::string const & in) {
-	return fmt::format("{}", "");
+	return fmt::format("{}", solve(in, 14));
 }
 
 }	// namespace aoc2022::day06
