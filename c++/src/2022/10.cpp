000bba25132ffaa8a86504cb1054234cf338e15e
Rohith Nallandigal <rnallandigal@gmail.com>
2022-12-12 22:05:40 -0500

    [C++] Day 10, 2022
    Signed-off-by: Rohith Nallandigal <rnallandigal@gmail.com>

--- c++/src/2022/10.cpp
+++ c++/src/2022/10.cpp
@@ -1,9 +1,5 @@
 #include <vector>
 #include <string>
-#include <unordered_map>
-#include <unordered_set>
-#include <utility>
-#include <tuple>
 
 #include <fmt/format.h>
 
@@ -11,12 +7,40 @@
 
 namespace aoc2022::day10 {
 
+void run(std::string const & in, std::function<void(int, int)> const & cb) {
+	int cycle = 1, x = 1, dx = 0, latency = 0;
+	auto insts = aoc::split(in, "\n");
+	for(int i = 0; i < (int)insts.size() || latency; cycle++, latency--) {
+		if(latency == 0) {
+			x += dx;
+			if(insts[i].substr(0, 4) == "noop") {
+				latency = 1;
+				dx = 0;
+			} else if(insts[i].substr(0, 4) == "addx") {
+				latency = 2;
+				dx = std::stoi(insts[i].substr(5));
+			}
+			i++;
+		}
+		cb(cycle, x);
+	}
+}
+
 std::string part1(std::string const & in) {
-	return fmt::format("{}", "");
+	int score = 0;
+	run(in, [&score](int cycle, int x) {
+		score += cycle % 40 == 20 ? cycle * x : 0;
+	});
+	return fmt::format("{}", score);
 }
 
 std::string part2(std::string const & in) {
-	return fmt::format("{}", "");
+	std::string crt = "\n";
+	run(in, [&crt](int cycle, int x) {
+		crt.push_back(aoc::between(x - 1, x + 1, (cycle - 1) % 40) ? '#' : '.');
+		if(cycle % 40 == 0) crt.push_back('\n');
+	});
+	return crt;
 }
 
 }	// namespace aoc2022::day10
