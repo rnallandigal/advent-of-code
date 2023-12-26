11ac005816f7bda5914ae26485a78173829610cd
Rohith Nallandigal <rnallandigal@gmail.com>
2023-12-26 02:11:27 -0500

    AoC 2023 - cpp templates
    Signed-off-by: Rohith Nallandigal <rnallandigal@gmail.com>

---
+++ c++/src/2023/25.cpp
@@ -0,0 +1,18 @@
+#include <string>
+#include <tuple>
+#include <unordered_map>
+#include <unordered_set>
+#include <utility>
+#include <vector>
+
+#include <fmt/format.h>
+
+#include "utils.h"
+
+namespace aoc2023::day25 {
+
+std::string part1(std::string const& in) { return fmt::format("{}", ""); }
+
+std::string part2(std::string const& in) { return fmt::format("{}", ""); }
+
+} // namespace aoc2023::day25
